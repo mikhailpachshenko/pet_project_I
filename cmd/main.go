e194adece62dbe3ed9c56d8dd37869fad1d8a75a
mikhailpachshenko <mikhailpachshenko@gmail.com>
2024-05-23 17:05:39 +0500

    MERGE: some

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	nH "github.com/mikhailpachshenko/pet_project_I.git/handler"
+	nH "github.com/mikhailpachshenko/pet_project_I.git/handlers"
 	m "github.com/mikhailpachshenko/pet_project_I.git/models"
 
 	"github.com/gin-gonic/gin"
@@ -13,10 +13,10 @@ func main() {
 
 	router := gin.Default() // создаём объект типа *gin.Engine
 
-	router.POST("/employee")       // создание сотрудника
-	router.GET("/employee/:id")    // получение информации о сотруднике
-	router.PUT("/employee/:id")    // редактирование информации о сотруднике
-	router.DELETE("/employee/:id") // удаление сотрудника
+	router.POST("/employee", handler.CreateEmployee)       // создание сотрудника
+	router.GET("/employee/:id", handler.GetEmployee)       // получение информации о сотруднике
+	router.PUT("/employee/:id", handler.UpdateEmployee)    // редактирование информации о сотруднике
+	router.DELETE("/employee/:id", handler.DeleteEmployee) // удаление сотрудника
 
 	router.Run() // запуск HTTP сервера
 }
